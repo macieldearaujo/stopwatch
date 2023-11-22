679c9266bd33b4e34dd0426e16f11b017f2b6f87
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-22 15:36:22 -0300

    Add toLocaleTimeString

--- stopwatch.js
+++ stopwatch.js
@@ -2,103 +2,57 @@ const readParagraph = document.querySelector('.js-time');
 const readPlayButton = document.querySelector('.js-play-button');
 const readResetButton = document.querySelector('.js-reset-button');
 
+let sec = 0;
+let min = 0;
+let hour = 0;
 
+let date = new Date();
 
-const time = {
-    seconds: 0,
-    minutes: 0,
-    hours: 0
+function showHour(newDate) {
+    return newDate.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
 }
 
-console.log(time.hours, time.minutes, time.seconds)
-
-let allowExecution = false;
-let intervalID;
-
-showResults();
+let timer;
+let pause = true;
 
 readPlayButton.addEventListener('click', () => {
-    if (!allowExecution) {
-        clearInterval(intervalID);
-        allowExecution = true;
-        startTimer();
+    if (pause) {
+        readPlayButton.innerText = 'Play';
+        console.log('Started!');
+        playTimer();
+        pause = false;
     } else {
-        clearInterval(intervalID);
-        allowExecution = false;
-    }
-    allowExecution === false ? console.log('stopped') : console.log('run');
-
-    const pause = readPlayButton.classList.contains('css-pause-button');
-    if (!pause) {
-        readPlayButton.innerHTML = 'Pause';
-        readPlayButton.classList.add('css-pause-button');
-    } else {
-        readPlayButton.innerHTML = 'Play';
-        readPlayButton.classList.remove('css-pause-button');
+        readPlayButton.innerText = 'Pause'
+        console.log('Paused!')
+        pauseTimer();
     }
 });
 
-readResetButton.addEventListener('click', () => {
-    reset();
-});
-
-function startTimer() {
-    intervalID = setInterval(() => {
-        if (allowExecution === false) { //Check if the timer should stop
-            return;
-        };
-
-        if (time.seconds >= 59) {
-            time.seconds = 0;
-            time.minutes++;
-            consoleLogResult();
-        } else {
-            time.seconds++;
-            consoleLogResult();
-        }
-
-
-        if (time.minutes > 59) {
-            time.minutes = 0;
-            time.hours++;
-            consoleLogResult();
-        }
-
-        if (time.hours >= 24) {
-            consoleLogResult();
-            clearInterval(intervalID);
-            return;
-        }
-
-        showResults();
+function playTimer() {
+    timer = setInterval(() => {
+        sec++;
+        date.setHours(hour, min, sec);
+        console.log(showHour(date));
+        readParagraph.innerHTML = showHour(date);
     }, 1000);
 }
 
-function showResults() {
-    const html = `
-    ${time.hours.toString().padStart(2, '0')}:
-    ${time.minutes.toString().padStart(2, '0')}:
-    ${time.seconds.toString().padStart(2, '0')}
-    `
-    document.querySelector('.js-time').innerHTML = html;
+function pauseTimer() {
+    clearTimeout(timer);
+    pause = true;
 }
 
-function consoleLogResult() {
-    console.log(`${time.hours}, ${time.minutes}, ${time.seconds} ${typeof time.seconds}`);
-}
-
-function reset() {
-    time.hours = 0;
-    time.minutes = 0;
-    time.seconds = 0;
-    allowExecution = false;
-    clearInterval(intervalID);
-    console.log('Reset')
-    showResults();
-    const pause = readPlayButton.classList.contains('css-pause-button');
-
-    if(pause) {
-        readPlayButton.innerHTML = 'Play';
-        readPlayButton.classList.remove('css-pause-button');
-    }
-}
+readResetButton.addEventListener('click', () => {
+    clearTimeout(timer);
+    sec = 0;
+    pause = true;
+    date.setHours(0, 0, 0);
+    console.log('Reset!')
+    console.log(showHour(date));
+    readParagraph.innerHTML = showHour(date);
+});
