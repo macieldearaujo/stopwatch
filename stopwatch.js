c0872cf2b30fe8e2f52c916096017799565cc6a6
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-27 12:35:31 -0300

    simplify the code

--- stopwatch.js
+++ stopwatch.js
@@ -2,75 +2,76 @@ const readParagraph = document.querySelector('.js-time');
 const readPlayButton = document.querySelector('.js-play-button');
 const readResetButton = document.querySelector('.js-reset-button');
 
-const hours = JSON.parse(localStorage.getItem("hours")) || {
-    hour: 0,
-    min: 0,
-    sec: 0
-};
-
-console.log(hours)
-
-let date = new Date();
-
-date.setHours(hours.hour, hours.min, hours.sec);
-readParagraph.innerHTML = showHour(date);
-
-function showHour(newDate) {
-    return newDate.toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false
-    });
-}
-
+let sec = 0;
 let timer;
 let pause = true;
 
+function createHours(sec) {
+    const date = new Date(sec * 1000);
+    return date.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+function createSec() {
+    timer = setInterval(() => {
+        sec++;
+        readParagraph.innerHTML = createHours(sec);
+    }, 1000)
+}
+
 readPlayButton.addEventListener('click', () => {
-    if (pause) {
-        readPlayButton.innerText = 'Play';
-        console.log('Started!');
-        playTimer();
+    if(pause) {
+        readPlayButton.innerText = 'Pause'
+        readPlayButton.
+        createSec();
         pause = false;
     } else {
-        readPlayButton.innerText = 'Pause'
-        console.log('Paused!')
-        pauseTimer();
+        readPlayButton.innerText = 'Play'
+        clearInterval(timer);
+        pause = true;
     }
 });
 
 readResetButton.addEventListener('click', () => {
-    resetTimer();
+    pause = true;
 });
 
-function playTimer() {
-    timer = setInterval(() => {
-        hours.sec++;
-        date.setHours(hours.hour, hours.min, hours.sec);
-        console.log(showHour(date));
-        readParagraph.innerHTML = showHour(date);
-        saveToStorage()
-    }, 1000);
-}
+// readResetButton.addEventListener('click', () => {
+//     resetTimer();
+// });
 
-function pauseTimer() {
-    clearTimeout(timer);
-    pause = true;
-}
+// function playTimer() {
+//     timer = setInterval(() => {
+//         hours.sec++;
+//         date.setHours(hours.hour, hours.min, hours.sec);
+//         console.log(showHour(date));
+//         readParagraph.innerHTML = showHour(date);
+//         saveToStorage()
+//     }, 1000);
+// }
 
-function resetTimer() {
-    clearTimeout(timer);
-    hours.sec = 0;
-    hours.min = 0;
-    hours.hour = 0;
-    pause = true;
-    date.setHours(0, 0, 0);
-    console.log('Reset!');
-    readParagraph.innerHTML = showHour(date);
-    saveToStorage()
-}
+// function pauseTimer() {
+//     clearTimeout(timer);
+//     pause = true;
+// }
 
-function saveToStorage() {
-    localStorage.setItem("hours", JSON.stringify(hours));
-}
+// function resetTimer() {
+//     clearTimeout(timer);
+//     hours.sec = 0;
+//     hours.min = 0;
+//     hours.hour = 0;
+//     pause = true;
+//     date.setHours(0, 0, 0);
+//     console.log('Reset!');
+//     readParagraph.innerHTML = showHour(date);
+//     saveToStorage()
+// }
+
+// function saveToStorage() {
+//     localStorage.setItem("hours", JSON.stringify(hours));
+// }
